ea138b6c97e21077214617fabdc151b293a01809
deyner <152225147+deynergon@users.noreply.github.com>
2024-04-18 09:25:20 -0500

    Add files via upload

---
+++ trabajojaime-main/gonzalez/database/usuarios.js
@@ -0,0 +1,11 @@
+export let usuarios = [
+    {
+      nombre: "deyner gonzalez",
+      documento: "1045486681",
+      telefono: "3234611668",
+      direccion: "carrera 26 eg",
+      correo: "deinergonzales83@gmail.com.com",
+      nota: 4.5,
+    },
+    
+  ];
